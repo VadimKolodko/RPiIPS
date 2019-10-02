9803a2b4efe0721b524fac3bfcdaecc53b655238
Vadim Kolodko <kolodko98@mail.ru>
2019-10-02 18:41:40 +0300

    add Random Function

---
+++ randomLine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(randomWrite(1000));
+        }
+
+        static string randomWrite(int count)
+        {
+            string str = "";
+            Random random = new Random();
+
+            for(int i = 0; i < count; i++)
+            {
+                str += random.Next();
+            }
+
+            return str;
+        }
+    }
+}
